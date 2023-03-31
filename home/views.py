76598b2fdc13a5e72f7fed471e1db136f5748573
Avjeet@123 <avjeetyadav920@gmail.com>
2023-03-31 15:30:47 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'variable': "this is sent"
+    }
+    messages.success(request,"this is a test massage")
+    return render(request,'index.html', context)
+
+def about(request):
+    return render(request,"about.html")
+    # return HttpResponse('this is about page')
+
+
+def services(request):
+    return render(request,'services.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        contact = request.POST.get('contact')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, contact=contact, desc=desc, date= datetime.today())
+        contact.save()
+        messages.success(request, 'Your massage has been sent')
+        
+    return render(request,"contact.html")
